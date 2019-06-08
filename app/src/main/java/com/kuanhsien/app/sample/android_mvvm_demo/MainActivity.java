fcf67146a0fc802ba131b1ed82dc45ef81deaf0b
KuanHsien <coson002fy@gmail.com>
2019-06-08 18:46:12 +0800

    Initial commit

---
+++ app/src/main/java/com/kuanhsien/app/sample/android_mvvm_demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kuanhsien.app.sample.android_mvvm_demo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
