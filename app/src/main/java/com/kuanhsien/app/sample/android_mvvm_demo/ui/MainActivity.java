ee535b76ba70ebad0e00c81cc23b08222ca25aa9
KuanHsien <coson002fy@gmail.com>
2019-06-18 18:33:53 +0800

    2. Add Databinding in Main Page

--- app/src/main/java/com/kuanhsien/app/sample/android_mvvm_demo/ui/MainActivity.java
+++ app/src/main/java/com/kuanhsien/app/sample/android_mvvm_demo/ui/MainActivity.java
@@ -3,14 +3,11 @@ package com.kuanhsien.app.sample.android_mvvm_demo.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 import com.kuanhsien.app.sample.android_mvvm_demo.R;
-import com.kuanhsien.app.sample.android_mvvm_demo.data.InfoModel;
+import com.kuanhsien.app.sample.android_mvvm_demo.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -20,12 +17,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+        // ViewModel
         mViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
         mViewModel.setRepository(this);
 
-        setupView();
+        // DataBinding
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setViewModel(mViewModel);
     }
 
     @Override
@@ -35,40 +34,11 @@ public class MainActivity extends AppCompatActivity {
         mViewModel.prepareData("Cover");
     }
 
-    // setup view in onCreate
-    private void setupView() {
+    public void startItemListActivity(View view) {
+        startActivity(new Intent(MainActivity.this, ItemListActivity.class));
+    }
 
-        // 1. get view component
-        final TextView tvTitle = findViewById(R.id.tv_cover_title);
-        final TextView tvDesc = findViewById(R.id.tv_cover_desc);
-        final ImageView ivImage = findViewById(R.id.iv_cover_image);
-        final Button btnToItemPage = findViewById(R.id.btn_to_item_page);
-        final Button btnToInfoPage = findViewById(R.id.btn_to_info_page);
-
-        btnToItemPage.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, ItemListActivity.class));
-            }
-        });
-
-        btnToInfoPage.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, InfoActivity.class));
-            }
-        });
-
-        // 2. prepare viewModel and observe livedata
-        mViewModel.getInfo().observe(this, new Observer<InfoModel>() {
-            @Override
-            public void onChanged(InfoModel data) {
-
-                // set view
-                tvTitle.setText(data.getTitle());
-                tvDesc.setText(data.getDesc());
-                ivImage.setBackgroundResource(data.getImageRes());
-            }
-        });
-
-        mViewModel.prepareTestData();
+    public void startInfoActivity(View view) {
+        startActivity(new Intent(MainActivity.this, InfoActivity.class));
     }
 }
--- app/src/main/java/com/kuanhsien/app/sample/android_mvvm_demo/ui/MainViewModel.java
+++ app/src/main/java/com/kuanhsien/app/sample/android_mvvm_demo/ui/MainViewModel.java
@@ -2,27 +2,22 @@ package com.kuanhsien.app.sample.android_mvvm_demo.ui;
 
 import android.content.Context;
 import android.os.AsyncTask;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 import com.kuanhsien.app.sample.android_mvvm_demo.data.InfoModel;
 import com.kuanhsien.app.sample.android_mvvm_demo.data.InfoRepository;
-import com.kuanhsien.app.sample.android_mvvm_demo.data.ItemRepository;
 
 public class MainViewModel extends ViewModel {
 
     // Repository
-    private ItemRepository mItemRepository;
     private InfoRepository mInfoRepository;
 
-    void setRepository(Context context) {
-        mItemRepository = new ItemRepository(context);
+    public void setRepository(Context context) {
         mInfoRepository = new InfoRepository(context);
     }
 
-    void setRepository(ItemRepository itemRepository, InfoRepository infoRepository) {
-        mItemRepository = itemRepository;
+    public void setRepository(InfoRepository infoRepository) {
         mInfoRepository = infoRepository;
     }
 
@@ -30,7 +25,7 @@ public class MainViewModel extends ViewModel {
     // LiveData
     private MutableLiveData<InfoModel> mInfo;
 
-    LiveData<InfoModel> getInfo() {
+    public LiveData<InfoModel> getInfo() {
         if (mInfo == null) {
             mInfo = new MutableLiveData<>();
         }
@@ -38,13 +33,7 @@ public class MainViewModel extends ViewModel {
     }
 
 
-    // fun
-    void prepareTestData() {
-        mItemRepository.prepareTestItem();
-        mInfoRepository.prepareTestInfo();
-    }
-
-    void prepareData(final String itemId) {
+    public void prepareData(final String itemId) {
 
         AsyncTask.execute(new Runnable() {
             @Override
