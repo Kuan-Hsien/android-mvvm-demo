de07a5c4d66eee605cd4bb84cdaef6f4d5f55058
KuanHsien <coson002fy@gmail.com>
2019-06-18 20:36:23 +0800

    1. Add DemoApplication to prepare data and add shared-preference to record if is first time to login

---
+++ app/src/main/java/com/kuanhsien/app/sample/android_mvvm_demo/DemoApplication.java
@@ -0,0 +1,45 @@
+package com.kuanhsien.app.sample.android_mvvm_demo;
+
+import android.app.Application;
+import android.content.Context;
+import com.kuanhsien.app.sample.android_mvvm_demo.data.InfoRepository;
+import com.kuanhsien.app.sample.android_mvvm_demo.data.ItemRepository;
+import com.kuanhsien.app.sample.android_mvvm_demo.utils.Constants;
+
+public class DemoApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // [IS_FIRST_FLAG = true] means first login to this app
+        if (getSharedPreferences(Constants.FILE_USER_DATA, Context.MODE_PRIVATE)
+                .getBoolean(Constants.IS_FIRST_FLAG, true)) {
+            firstLogin();
+        }
+    }
+
+    public void firstLogin() {
+
+        // (1) prepare default data
+        prepareDatabase();
+
+        // (2) greeting page & tips
+        // TODO
+
+        // (3) once login, save is_first_flag = false into shared-preferences
+        getSharedPreferences(Constants.FILE_USER_DATA, Context.MODE_PRIVATE)
+                .edit()
+                .putBoolean(Constants.IS_FIRST_FLAG, false)
+                .apply();
+    }
+
+    private void prepareDatabase() {
+
+        InfoRepository infoRepository = new InfoRepository(this);
+        ItemRepository itemRepository = new ItemRepository(this);
+
+        infoRepository.prepareTestInfo();
+        itemRepository.prepareTestItem();
+    }
+}
--- app/src/main/java/com/kuanhsien/app/sample/android_mvvm_demo/utils/Constants.java
+++ app/src/main/java/com/kuanhsien/app/sample/android_mvvm_demo/utils/Constants.java
@@ -8,6 +8,10 @@ public class Constants {
     // Bundle Key
     public static final String BUNDLE_KEY_ITEM_ID = "itemKeyId";
 
+    // SharePreference
+    public static final String FILE_USER_DATA = "FILE_USER_DATA";
+    public static final String IS_FIRST_FLAG = "IS_FIRST_FLAG";
+
     // Database
     public static final String DATABASE_NAME = "android-mvvm-demo-db";
 }
